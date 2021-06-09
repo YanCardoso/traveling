96255b52d357eedc040f10a852f1fb22a450489e
Yan Cardoso <yancard7@gmail.com>
2021-06-08 22:56:08 -0300

    add section 02

--- src/components/ContentBody/index.js
+++ src/components/ContentBody/index.js
@@ -27,8 +27,32 @@ function ContentBody() {
         </div>
         <img id="img01" src="./assets/01.png" />
       </section>
-      <section></section>
-      <section></section>
+
+      {/* Section 02 */}
+
+      <section id="section--02">
+        <span id="tag--number02">02</span>
+        <div className="content--body--main--2">
+          <div className="tag--name">
+            <img src="./assets/Rectangle.png" />
+            <span>Hiking Essentials</span>
+          </div>
+          <h1>Picking the right Hiking Gear!</h1>
+          <p>
+            The nice thing about beginning hiking is that you don’t really need
+            any special gear, you can probably get away with things you already
+            have. Let’s start with clothing. A typical mistake hiking beginners
+            make is wearing jeans and regular clothes, which will get heavy and
+            chafe wif they get sweaty or wet.
+          </p>
+          <div className="tag--footer">
+            <span>Read more</span>
+            <img src="./assets/icon-navigation-arrow_downward_24px.png" />
+          </div>
+        </div>
+        <img id="img02" src="./assets/02.png" />
+      </section>
+      <section id="section--03"></section>
     </div>
   );
 }
