37782f19f1fcfc666cc3cac6b3240c8f23f59e14
Yan Cardoso <yancard7@gmail.com>
2021-06-18 16:43:46 -0300

    add parallax effect

--- src/components/BgHero/index.js
+++ src/components/BgHero/index.js
@@ -1,22 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../styles/components/BgHero.css";
 
 function BgHero() {
+  const [scroll, setScroll] = useState();
+
+  function parallax() {
+    let winScroll = window.pageYOffset
+    setScroll(winScroll)
+    const translate = document.querySelectorAll(".translate")
+    translate.forEach(element => {
+      let speed = element.dataset.speed;
+      element.style.transform = `translateY(${-scroll * speed}px)`;
+    })
+
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", parallax)
+    return () => {
+      window.removeEventListener("scroll", parallax)
+    };
+  }, [scroll]);
+
+
   return (
     <div className="bgHero" id="hero">
-      <div className="HG" style={{ backgroundImage: "url('/assets/HG.png')" }}>
-        <div className="content-shadow"></div>
-      </div>
-      <div
-        className="MG"
-        style={{ backgroundImage: "url('/assets/MG.png')" }}
-      ></div>
-      <div
-        className="VG"
-        style={{ backgroundImage: "url('/assets/VG.png')" }}
-      ></div>
-
-      <div className="bg-footer-shadow"></div>
+      <div className="content-shadow"></div>
+      <img
+        className="HG translate"
+        src="./assets/HG.png"
+        alt=""
+        data-speed="-0.2"
+      />
+      <img
+        className="MG translate"
+        src="./assets/MG.png"
+        alt=""
+        data-speed="0.3"
+      />
+      <img
+        className="VG translate"
+        src="./assets/VG.png"
+        alt=""
+        data-speed="0.2"
+      />
+      <div className="bg-footer-shadow translate" data-speed="0.2"></div>
     </div>
   );
 }
